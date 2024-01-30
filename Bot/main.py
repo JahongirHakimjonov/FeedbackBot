1b8385310cb1412ad6a62ccee76ac7399191a04a
Jahongir <jahongirhakimjonov@gmail.com>
2024-01-30 19:44:58 +0500

    fix codes

--- Bot/main.py
+++ Bot/main.py
@@ -8,31 +8,25 @@ from aiogram.utils import executor
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from dotenv import load_dotenv, find_dotenv
 
-# Load environment variables
 load_dotenv(find_dotenv())
 
-# Initialize bot and dispatcher
 bot_token = os.getenv('BOT_TOKEN')
 if not bot_token:
     raise ValueError("Missing BOT_TOKEN environment variable")
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-# Database credentials
 dbname = os.getenv('DB_NAME')
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
 host = os.getenv('DB_HOST')
 
-# Connect to the PostgreSQL database
 conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-# Create a cursor object
 cur = conn.cursor()
 
 
-# Define states for FSM
 class Form(StatesGroup):
     login_id = State()
     password = State()
@@ -40,15 +34,11 @@ class Form(StatesGroup):
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    # Create a keyboard
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # Add 'info' button
     keyboard.add(types.KeyboardButton('Infoℹ⁉️'))
-    # Send a message with this keyboard
-    await bot.send_message(message.chat.id, "👋👋👋", reply_markup=keyboard)
+    await bot.send_message(message.chat.id, "👀", reply_markup=keyboard)
 
     try:
-        # Check if telegram_id already exists in the database
         cur.execute("SELECT * FROM students WHERE telegram_id = %s", (message.from_user.id,))
         result = cur.fetchone()
         if result is None:
@@ -83,12 +73,10 @@ async def process_password(message: types.Message, state: FSMContext):
     try:
         async with state.proxy() as data:
             data['password'] = message.text
-            # Check if login_id and password exist in the database
             cur.execute("SELECT * FROM students WHERE login_id = %s AND password = %s",
                         (data['login_id'], data['password']))
             result = cur.fetchone()
             if result is not None:
-                # Update telegram_id in the database
                 cur.execute("UPDATE students SET telegram_id = %s WHERE login_id = %s AND password = %s",
                             (message.from_user.id, data['login_id'], data['password']))
                 conn.commit()
@@ -107,7 +95,6 @@ async def process_password(message: types.Message, state: FSMContext):
 @dp.message_handler(commands='register')
 async def cmd_register(message: types.Message):
     try:
-        # Check if the user is not already registered
         cur.execute("SELECT * FROM students WHERE telegram_id = %s", (message.from_user.id,))
         result = cur.fetchone()
         if result is None:
@@ -140,7 +127,6 @@ async def process_login_id(message: types.Message, state: FSMContext):
 
 @dp.message_handler(lambda message: message.text == 'Infoℹ⁉️')
 async def handle_info_button(message: types.Message):
-    # The same code as in your cmd_info function
     try:
         cur.execute(
             "SELECT students.first_name, students.last_name, groups.group_num, students.course_num, "
@@ -167,6 +153,5 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"Error in polling: {e}")
     finally:
-        # Close cursor and connection
         cur.close()
         conn.close()
--- feedbackbot/models.py
+++ feedbackbot/models.py
@@ -54,7 +54,7 @@ class Student(AbstractBaseModel):
     password = models.CharField(max_length=20, unique=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    telegram_id = models.BigIntegerField(unique=False, blank=True, null=True)
+    telegram_id = models.BigIntegerField(unique=True, blank=True, null=True)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='students')
     course_num = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
     is_active = models.BooleanField(default=True)
