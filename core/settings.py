43997e8283c9b4c54bed2bd8a7eef9dad4b18106
Jahongir <jahongirhakimjonov@gmail.com>
2024-02-20 18:05:59 +0500

    static settings updated

--- core/settings.py
+++ core/settings.py
@@ -134,11 +134,9 @@ USE_TZ = True
 
 # settings.py
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_DIRS = [
-    str(BASE_DIR.joinpath('static'))
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
