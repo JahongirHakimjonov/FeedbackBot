66941630a359eb58a32ae6eb8de04a332544f0a6
Jahongir <jahongirhakimjonov@gmail.com>
2024-02-25 00:50:58 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['feedback.tiiame.uz']
+ALLOWED_HOSTS = ['*']
 
 # CSRF_TRUSTED_ORIGINS = ['feedback.tiiame.uz']
 
