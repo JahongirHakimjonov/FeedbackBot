7a9426b178687036ec771e464be7b0cea0e6f3da
Jahongir <jahongirhakimjonov@gmail.com>
2024-03-08 11:58:43 +0500

    Support connect PSQL

--- Support/main.py
+++ Support/main.py
@@ -50,7 +50,7 @@ conn, c = setup_database()
 # Create a PostgreSQL table to store user details
 c.execute('''
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         full_name TEXT,
         username TEXT,
         telegram_id INTEGER
